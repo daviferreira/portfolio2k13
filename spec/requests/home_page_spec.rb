45f7550a4e26fc7ce5081ceacd07215ad24b366f
Davi Ferreira <davi.ferreira@corp.globo.com>
2013-02-08 10:00:44 -0200

    Adds new placeholder items to user menu

--- spec/requests/home_page_spec.rb
+++ spec/requests/home_page_spec.rb
@@ -14,9 +14,16 @@ describe 'Home Page' do
     end
 
     it { should have_selector('a.logout', :text => "#{I18n.t('logout')}") }
+
     it { should have_selector('ul > li > a[href="' + root_path + '"]', :text => "#{I18n.t('home')}") }
+
     it { should have_selector('ul > li > a[href="' + projects_path + '"]', :text => "#{I18n.t('projects.list')}") }
     it { should have_selector('ul > li > a[href="' + new_project_path + '"]', :text => "#{I18n.t('projects.add')}") }
+
+    it { should have_selector('ul > li > a[href="#"]', :text => "#{I18n.t('categories.list')}") }
+    it { should have_selector('ul > li > a[href="#"]', :text => "#{I18n.t('tags.list')}") }
+    it { should have_selector('ul > li > a[href="#"]', :text => "#{I18n.t('photos.list')}") }
+
   end
 
 end
