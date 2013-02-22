64bdab250f80b7e647b1187bf1bcfc51f0f778ed
Davi Ferreira <contato@daviferreira.com>
2013-02-22 14:27:50 -0300

    Implements posts controller specs

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,26 +2,43 @@ class PostsController < ApplicationController
   before_filter :authenticate_user!, :except => [:show]
   layout "admin", :except => [:show]
 
-  def new
-    @post = Post.new
-  end
-
-  def create
-  end
-
-  def update
-  end
-
-  def edit
-  end
-
-  def destroy
-  end
-
   def index
     @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    if @post.save
+      redirect_to posts_path, :flash => { :success => t("posts.created") }
+    else
+      render "posts/new"
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(params[:post])
+      redirect_to edit_post_path(@post), :flash => { :success => t("posts.updated") }
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, :flash => { :notice => t("posts.deleted") }
   end
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -45,19 +45,144 @@ describe PostsController do
     describe "GET 'index'" do
 
       it "returns http success" do
-	get :index
-	response.should be_success
+        get :index
+        response.should be_success
       end
 
       it "should list all the posts" do
-	p1 = FactoryGirl.create(:post, :title => "Foo bar")
-	p2 = FactoryGirl.create(:post, :title => "Baz quux")
-	get :index
-	response.body.should have_selector('td', :text => p1.title)
-	response.body.should have_selector('td', :text => p2.title)
+        p1 = FactoryGirl.create(:post, :title => "Foo bar")
+        p2 = FactoryGirl.create(:post, :title => "Baz quux")
+        get :index
+        response.body.should have_selector('td', :text => p1.title)
+        response.body.should have_selector('td', :text => p2.title)
+      end
+
+      it "should paginate the posts" do
+        pending "implement will paginate"
       end
 
     end
+
+    describe "create post" do
+
+      describe "GET 'new'" do
+        it "returns http success" do
+          get :new
+          response.should be_success
+        end
+      end
+
+      describe "POST 'create'" do
+        it "should changes post count by 1" do
+          expect {
+            post :create, :post => {:title => "Test post",
+                                    :abstract => "Post description",
+                                    :body => "Post body"}
+          }.to change(Post, :count).by(1)
+        end
+      end
+
+      describe "with invalid post data" do
+        it "should render new post template" do
+          post :create, :post => Post.new(title: "")
+          response.should render_template("posts/new")
+        end
+      end
+
+      describe "with valid post data" do
+        it "should redirect to posts list" do
+          post :create, :post => {:title => "Test post",
+                                  :abstract => "Description",
+                                  :body => "Post body"}
+          response.should redirect_to posts_path
+        end
+      end
+
+    end
+
+    describe "edit post" do
+
+      describe "GET 'edit'" do
+        it "should return 404 when id is invalid" do
+          get :edit, :id => "invalid"
+          response.response_code.should == 404
+        end
+
+        it "returns http success" do
+          get :edit, :id => blog_post
+          response.should be_success
+        end
+      end
+
+      describe "PUT 'update'" do
+
+        describe "with invalid post data" do
+          it "should return 404 when id is invalid" do
+            post :update, :id => "invalid"
+            response.response_code.should == 404
+          end
+
+          it "should render edit post template" do
+            post :update, :id => blog_post, :post => {:title => ""}
+            response.should render_template("posts/edit")
+          end
+        end
+
+        describe "with valid post data" do
+          it "redirects to post path" do
+            put :update, :id => blog_post, :post => {:title => "Editing post"}
+            response.should redirect_to edit_post_path(blog_post)
+          end
+
+          it "changes the post title" do
+            put :update, :id => blog_post, :post => {:title => "Editing post"}
+            blog_post.reload
+            blog_post.title.should == "Editing post"
+          end
+
+        end
+
+      end
+
+    end
+
+    describe "destroy post" do
+
+      describe "DELETE 'destroy'" do
+
+        it "should return 404 when id is invalid" do
+          delete :destroy, :id => "invalid"
+          response.response_code.should == 404
+        end
+
+        it "should redirect to the post listing path" do
+          delete :destroy, :id => blog_post
+          response.should redirect_to posts_path
+        end
+
+        it "should delete a post" do
+          p1 = FactoryGirl.create(:post, :title => "Sample Post")
+          expect { delete :destroy, :id => p1 }.to change(Post, :count).by(-1)
+        end
+
+      end
+
+    end
+
+    describe "show post" do
+
+      describe "GET 'show'" do
+
+        it "should return 404 when post is invalid" do
+          get :show, :id => "invalid"
+          response.response_code.should == 404
+        end
+
+      end
+
+    end
+
+
 
   end
 
