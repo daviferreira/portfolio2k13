b21fea8b37cb81772471f459047ce4cfe58e58a3
Davi Ferreira <davi.ferreira@corp.globo.com>
2013-03-11 14:17:06 -0300

    Changes create translation migrations to avoid errors with cached_slug

--- db/migrate/20130223000723_create_translations.rb
+++ db/migrate/20130223000723_create_translations.rb
@@ -1,5 +1,7 @@
 class CreateTranslations < ActiveRecord::Migration
   def up
+    Project.translated_attribute_names = [:name, :description, :tags]
+    Category.translated_attribute_names = [:name]
     Project.create_translation_table!({:name => :string, :description => :text, :tags => :string}, {:migrate_data => true})
     Category.create_translation_table!({:name => :string}, {:migrate_data => true})
     Photo.create_translation_table!({:title => :string}, {:migrate_data => true})
