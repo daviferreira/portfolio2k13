c35e6c3812f32fdeed805cdf432dd50eddbde717
Davi Ferreira <davi.ferreira@corp.globo.com>
2013-03-13 14:30:19 -0300

    Adds routing specs

---
+++ spec/routing/locale_spec.rb
@@ -0,0 +1,55 @@
+require 'spec_helper'
+
+describe "Locale routing" do
+
+  describe "Projects" do
+    it "should route to project with english locale" do
+      { :get => "/en/projects/test-project" }.
+          should route_to(
+          :locale => "en",
+          :controller => "projects",
+          :action => "show",
+          :id => "test-project"
+        )
+    end
+
+    it "should not accept an invalid locale" do
+      { :get => "/fr/projects/test-project" }.should_not be_routable
+    end
+
+    it "should accept no locale" do
+      { :get => "/projects/test-project" }.
+          should route_to(
+          :controller => "projects",
+          :action => "show",
+          :id => "test-project"
+        )
+    end
+  end
+
+  describe "Categories" do
+    it "should route to category with english locale" do
+      { :get => "/en/categories/test-category" }.
+          should route_to(
+          :locale => "en",
+          :controller => "categories",
+          :action => "show",
+          :id => "test-category"
+        )
+    end
+
+    it "should not accept an invalid locale" do
+      { :get => "/fr/categories/test-category" }.should_not be_routable
+    end
+
+    it "should accept no locale" do
+      { :get => "/categories/test-category" }.
+          should route_to(
+          :controller => "categories",
+          :action => "show",
+          :id => "test-category"
+        )
+    end
+  end
+
+end
