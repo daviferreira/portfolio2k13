656cd37f486c5c98a37e8fe5039043f669348782
Davi Ferreira <contato@daviferreira.com>
2013-03-17 18:37:09 -0300

    Adds new styles to titles and projects

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def index
-    @projects = Project.with_translations(I18n.locale).limit(15)
+    @projects = Project.with_translations(I18n.locale).limit(9)
     @posts = Post.where(:locale => I18n.locale)
     @categories = Category.with_translations(I18n.locale)
   end
