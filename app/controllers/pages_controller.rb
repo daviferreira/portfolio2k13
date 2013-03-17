52974ec7bbcd04bc7148aa28226650f6fba9da99
Davi Ferreira <contato@daviferreira.com>
2013-03-17 16:55:19 -0300

    Removes simple grid, implements initial black & white layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def index
-    @projects = Project.with_translations(I18n.locale)
+    @projects = Project.with_translations(I18n.locale).limit(15)
     @posts = Post.where(:locale => I18n.locale)
     @categories = Category.with_translations(I18n.locale)
   end
