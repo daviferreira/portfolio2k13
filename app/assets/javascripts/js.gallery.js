1f5f4ac5e374e854f20355c522949124da1a1368
Davi Ferreira <contato@daviferreira.com>
2013-05-21 22:23:50 -0300

    Converte rvmrc para ruby-version e ruby-gemset

---
+++ app/assets/javascripts/js.gallery.js
@@ -0,0 +1,112 @@
+/*jslint browser:true */
+/*global jQuery */
+
+var JSGallery = JSGallery || {};
+
+(function ($, window, undefined) {
+    'use strict';
+
+    JSGallery = {
+        init: function () {
+            this.doc = $(document);
+            this.root = $('.js-gallery');
+            this.overlay = $('#js-gallery-overlay');
+            this.images = [];
+            this.bindActions();
+        },
+
+        bindActions: function () {
+            this.bindClick();
+            this.bindResize();
+        },
+
+        bindClick: function () {
+            var self = this;
+            this.root.find('a').click(function (e) {
+                e.preventDefault();
+                self.currentPhoto = $(this).attr('href');
+                self.showOverlay();
+            });
+        },
+
+        showOverlay: function () {
+            if (this.overlay.length === 0) {
+                this.overlay = this.createOverlay();
+            }
+            this.overlay.css('height', this.doc.height() + 'px').fadeIn('fast');
+            this.showImage();
+        },
+
+        createOverlay: function () {
+            var overlay = $('<div id="js-gallery-overlay" class="js-gallery-overlay" />'),
+                self = this;
+            overlay.click(function (e) {
+                e.preventDefault();
+                self.close();
+            });
+            return overlay.prependTo('body');
+        },
+
+        close: function () {
+            this.overlay.fadeOut('fast');
+            $('.js-gallery-image').fadeOut('fast');
+        },
+
+        showImage: function () {
+            var img,
+                h = $(window).height() - 300,
+                w;
+            if (this.images.indexOf(this.currentPhoto) === -1) {
+                this.images.push(this.currentPhoto);
+                img = new Image();
+                img.src = this.currentPhoto;
+                img.onload = function () {
+                    if (h > this.height) {
+                        h = this.height;
+                    }
+                    w = Math.ceil(this.width / this.height * h);
+                    $('<img src="' + this.src + '" class="js-gallery-image" />')
+                        .appendTo('body')
+                        .css({
+                            'display': 'none',
+                            'margin-left': -(w/2),
+                            'margin-top': -(h/2),
+                            'width': w,
+                            'height': h
+                        })
+                        .fadeIn('fast');
+                }
+            } else {
+                img = $('img[src*="' + this.currentPhoto + '"]');
+                if (h > img.height()) {
+                    h = img.height();
+                }
+                w = Math.ceil(img.width() / img.height() * h);
+                img.css({
+                    'display': 'none',
+                    'margin-left': -(w/2),
+                    'margin-top': -(h/2),
+                    'width': w,
+                    'height': h
+                }).fadeIn('fast');
+            }
+        },
+
+        bindResize: function () {
+            var self = this;
+            $(window).on('resize.js_gallery', function () {
+                self.overlay.css({
+                    'width': self.doc.width() + 'px',
+                    'height': self.doc.height() + 'px'
+                });
+            });
+        }
+
+    };
+
+}(jQuery, window));
+
+
+$(function () {
+    JSGallery.init();
+});
