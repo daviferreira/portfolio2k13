d0d8137933ab6399e838a64c8be6b727848d14de
Davi Ferreira <davi.ferreira@corp.globo.com>
2013-06-19 17:33:57 -0300

    Adds real posts and open source projects to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,8 @@
 class PagesController < ApplicationController
   def index
     @projects = Project.published.with_translations(I18n.locale).limit(9)
-    @posts = Post.where(:locale => I18n.locale)
+    @posts = Post.published.where(:locale => I18n.locale).limit(5);
+    @open_source_projects = OpenSourceProject.all.sample(2)
     @categories = Category.with_translations(I18n.locale)
     @has_footer = true
   end
