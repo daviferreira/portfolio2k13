01366c12bd3137b6b35537acc7033b05ac978df2
Davi Ferreira <contato@daviferreira.com>
2013-06-23 18:57:08 -0300

    Adds open source pages styles

--- app/controllers/open_source_projects_controller.rb
+++ app/controllers/open_source_projects_controller.rb
@@ -1,5 +1,5 @@
 class OpenSourceProjectsController < ApplicationController
   def index
-    @open_source_projects = OpenSourceProject.all
+    @open_source_projects = OpenSourceProject.with_translations(I18n.locale)
   end
 end
