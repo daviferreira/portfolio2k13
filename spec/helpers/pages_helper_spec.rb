069e407645944e6a3bef8053d602c70e0a535026
Davi Ferreira <contato@daviferreira.com>
2013-06-25 19:34:02 -0300

    Resets I18n default

--- spec/helpers/pages_helper_spec.rb
+++ spec/helpers/pages_helper_spec.rb
@@ -1,7 +1,11 @@
 require 'spec_helper'
 
 describe PagesHelper do
+
+  after { I18n.locale = I18n.default_locale }
+
   let!(:page) { FactoryGirl.create(:page, :body => '<p>Hello world!</p>') }
+
   describe "render page" do
     it "should render the page html" do
       expect helper.render_page('test-page').should == '<p>Hello world!</p>'
