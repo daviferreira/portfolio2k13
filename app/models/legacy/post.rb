eb96278c8c10fce8e3aab962665702bde37135c0
Davi Ferreira <contato@daviferreira.com>
2013-06-24 23:04:02 -0300

    Improves comment importing script

--- app/models/legacy/post.rb
+++ app/models/legacy/post.rb
@@ -1,4 +1,5 @@
 class Legacy::Post < Legacy::Base
   has_many :post_categories, :dependent => :destroy
   has_many :categories, :through => :post_categories
+  has_many :comments
 end
--- script/wxr_export.rb
+++ script/wxr_export.rb
@@ -3,6 +3,7 @@ require File.expand_path('../../config/boot', __FILE__)
 require File.expand_path('../../config/environment', __FILE__)
 
 # script/wxr_export.rb
+# TODO: strip HTML, converting br to new line
 
 xml = Builder::XmlMarkup.new(:target => STDOUT, :indent => 2)
 
@@ -15,50 +16,64 @@ xml.rss 'version' => "2.0",
         'xmlns:wp' => "http://wordpress.org/export/1.0/" do
 
   xml.channel do
-    Legacy::Comment.where(:published => true).each do |comment|
+      Legacy::Post.where(:published => true).each do |post|
 
-      xml.item do
+        if not post.tableless.present?
 
-        xml.wp(:comment) do
+        xml.item do
 
-          xml.wp(:comment_id) { |x| x << comment.id }
+          xml.title { |x| x << post.title }
+          xml.link { |x| x << "http://www.daviferreira.com/posts/#{post.cached_slug}" }
+          xml.content(:encoded) { |x| x << "<![CDATA[" + post.body.encode(:xml => :text).gsub('--', '- -').gsub('--', '- -').chars.select{|i| i.valid_encoding?}.join + "]]>" }
+          xml.dsq(:thread_identifier) { |x| x << post.id }
+          xml.wp(:post_date_gmt) { |x| x << post.created_at.utc.to_formatted_s(:db) }
+          xml.wp(:comment_status) { |x| x << "open" }
 
-          xml.wp(:comment_author) do |x|
-            x << comment.author
-          end
 
-          xml.wp(:comment_author_email) do |x|
-            x << comment.email
-          end
+          post.comments.where(:published => true).each do |comment|
 
-          xml.wp(:comment_author_url) do |x|
-            if comment.url.start_with?('http')
-              x << comment.url
-            else
-              x << ""
-            end
-          end
+            xml.wp(:comment) do
 
-          xml.wp(:comment_author_IP) { |x| x << comment.ip }
+              xml.wp(:comment_id) { |x| x << comment.id }
 
-          xml.wp(:comment_date_gmt) { |x| x << comment.created_at.utc.to_formatted_s(:db) }
+              xml.wp(:comment_author) do |x|
+                x << comment.author
+              end
 
-          xml.wp(:comment_content) { |x| x << "<!--[CDATA[" + comment.body.encode(:xml => :text).gsub('--', '- -').gsub('--', '- -').chars.select{|i| i.valid_encoding?}.join + "]]-->" }
+              xml.wp(:comment_author_email) do |x|
+                x << comment.email
+              end
 
-          xml.wp(:comment_approved) { |x| x << 1 } #approve all comments
+              xml.wp(:comment_author_url) do |x|
+                if comment.url.start_with?('http')
+                  x << comment.url
+                else
+                  x << ""
+                end
+              end
 
-          xml.wp(:comment_parent) do |x|
-            if comment.comment.present?
-              x << comment.comment.id
-            else
-              x << 0
-            end
-          end
+              xml.wp(:comment_author_IP) { |x| x << comment.ip }
 
-        end #xml.wp(:comment)
+              xml.wp(:comment_date_gmt) { |x| x << comment.created_at.utc.to_formatted_s(:db) }
 
-      end   #xml.item
+              xml.wp(:comment_content) { |x| x << "<![CDATA[" + comment.body.encode(:xml => :text).gsub('--', '- -').gsub('--', '- -').chars.select{|i| i.valid_encoding?}.join + "]]>" }
 
-    end       #Comments.all.each
+              xml.wp(:comment_approved) { |x| x << 1 } #approve all comments
+
+              xml.wp(:comment_parent) do |x|
+                if comment.comment.present?
+                  x << comment.comment.id
+                else
+                  x << 0
+                end
+              end
+
+            end #xml.wp(:comment)
+
+          end       #Comments.all.each
+
+        end   #xml.item
+        end #tableless
+    end #Post.all.each
   end         #xml.channel
 end           #xml.rss
