f2c486461f6258ed0b0dc19ceb24edbb8a2fbe03
Davi Ferreira <davi.ferreira@corp.globo.com>
2013-06-27 09:46:53 -0300

    Adjusts open source page style

--- app/controllers/open_source_projects_controller.rb
+++ app/controllers/open_source_projects_controller.rb
@@ -1,5 +1,7 @@
 class OpenSourceProjectsController < ApplicationController
   def index
     @open_source_projects = OpenSourceProject.with_translations(I18n.locale)
+    @meta_title = t "open_source_projects.meta.title"
+    @meta_description = t "open_source_projects.meta.description"
   end
 end
