337bd46c5a98b970a56308552c88bdf7bf143169
Davi Ferreira <davi.ferreira@corp.globo.com>
2013-06-27 10:50:46 -0300

    Adds new translations, removes dummy images

--- spec/controllers/open_source_projects_controller_spec.rb
+++ spec/controllers/open_source_projects_controller_spec.rb
@@ -7,6 +7,12 @@ describe OpenSourceProjectsController do
       get 'index'
       response.should be_success
     end
+
+    it "should have the right metas" do
+      get :index
+      assigns(:meta_title).should == I18n.t('open_source_projects.meta.title')
+      assigns(:meta_description).should == I18n.t('open_source_projects.meta.description')
+    end
   end
 
 end
