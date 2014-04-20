12bb3ac7beecc62db11d2f03e7474533b68cb832
Davi Ferreira <contato@daviferreira.com>
2014-04-20 17:26:01 +1000

    Implements spaceless, minify disquss

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,16 @@ module ApplicationHelper
     def current_locale
         I18n.locale if I18n.locale != I18n.default_locale
     end
+
+    # http://stackoverflow.com/a/6925495
+    # Strip all whitespace between the HTML tags in the passed block, and
+    # on its start and end.
+    def spaceless(&block)
+      contents = capture(&block)
+
+      # Note that string returned by +capture+ is implicitly HTML-safe,
+      # and this mangling does not introduce unsafe changes, so I'm just
+      # resetting the flag.
+      contents.strip.gsub(/>\s+</, '><').html_safe
+    end
 end
